f5a88a1c4b0e4e2617d12fbdf07c25c6f697b2c3
guipleite <guipleite@gmail.com>
2019-09-27 10:23:14 -0300

    ROM E base do UC

---
+++ uc.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity controlunit is
+    port(
+        clk : in std_logic;
+        enable : in std_logic;
+        flag : in std_logic;
+		  inst : in std_logic_vector(19 downto 0);
+        mux1, mux2 : out std_logic;
+        we : out std_logic
+		  );
+end entity;
+
+architecture description of controlunit is
+
+begin
