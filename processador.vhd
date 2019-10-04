9c8673975fbc45188b2f7ec643c8aa7154377326
Veguinho <rafaelvr@al.insper.edu.br>
2019-10-04 10:58:21 -0300

    processador

--- processador.vhd
+++ processador.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 
 entity processador is
-               generic 
+   generic 
 	(
 		DATA_WIDTH : natural := 21;
 		ADDR_WIDTH : natural := 8
@@ -28,20 +28,17 @@ architecture description of processador is
 	signal ula_instr ,select_mux2 : std_logic_vector(1 downto 0);
 	signal mux1_out,mux2_out,addr_out,s1,s2,ula_out: std_logic_vector(ADDR_WIDTH-1 downto 0) ;
 	signal pc_out : std_logic_vector(ADDR_WIDTH-1 downto 0);
-	
+	begin
 	uc: entity work.uc
-		Port Map(
-		  clk <=clk;
-        enable <=enable_uc;
-		  op <=rom_in(ADDR_WIDTH-1 downto 17);                    --instrucao opcode
-        mux1=> select_mux1;									--instrucao para a ULA
-		  opr=>ula_instr ;
-		  mux2 =>select_mux2;   
-        we =>we_uc;                    					--Write enable no banco de registradores
-		  rw =>rw                     						--Write ou read
+		port map(
+		  clk => clk,
+		  op => rom_in(ADDR_WIDTH-1 downto 17),                    --instrucao opcode
+        mux1 => select_mux1,									--instrucao para a ULA
+		  opr=>ula_instr,
+		  mux2 =>select_mux2,  
+        we =>we_uc,                  					--Write enable no banco de registradores
+		  rw =>rw,		  --Write ou read
+		  flag => flag_uc
 		);
-	
-	
-	
 	
 end description;
