6f891b83445755700e3473fb3a73ca0c16ffb1ad
hugoecarl <mormeme@gmail.com>
2019-10-05 16:45:39 -0300

    parser

---
+++ assemblyparser/parser.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# author: Hugo Carl <mormeme@gmail.com>
+
+class Parser:
+    def __init__(self):
+        self.arquivo = open("assembly.txt", "r")
+        self.dicinst = {"mov":"0000","add":"0001","comp":"0010","je":"0011","jmp":"0100","compi":"0110","write":"1100","read":"0101","compr":"1110"}
+        self.dicregs = {"%su":"000","%sd":"001","%mu":"010","%md":"011","%hu":"100","%hd":"101","%tmp1":"110","%tmp2":"111"}
+        self.label = {}
+        self.bina = []
+
+    def readline(self):
+        a = []
+        count = 0
+        
+        for i in self.arquivo:
+            k = i.replace('$', '').replace(',', '').replace(':', '').split()
+            a.append(k)
+            if len(k) == 1:
+                self.label[k[0]] = count 
+            else:
+                count += 1
+        print(a)
+        for j in a:
+            if j[0] == "mov" or j[0] == "read":
+                self.bina.append(self.dicinst[j[0]] + "000000" + self.dicregs[j[1]] + self.hextobin8(j[2]))
+            elif j[0] == "write" or j[0] == "comp" or j[0] == "add":
+                self.bina.append(self.dicinst[j[0]] + "000" + self.dicregs[j[1]] + "000" + self.hextobin8(j[2]))
+            elif j[0] == "je" or j[0] == "jmp":
+                self.bina.append(self.dicinst[j[0]] + "000000000" + str(bin(self.label[j[1]])[2:].zfill(8)))
+            elif j[0] == "compi":
+                self.bina.append(self.dicinst[j[0]] + "000" + self.dicregs[j[1]] + self.dicregs[j[3]] + self.hextobin8(j[2]))
+            elif j[0] == "compr":
+                self.bina.append(self.dicinst[j[0]] + self.dicregs[j[1]] + self.dicregs[j[2]] + "00000000000")
+        
+        
+        print(self.bina)
+        print(self.label)
+        
+        self.write_file()
+    
+    def hextobin8(self, hex):
+        return str(bin(int(hex, 16))[2:].zfill(8))
+
+    def write_file(self):
+        output_file = open('binary.txt','w')
+        for i in self.bina:
+            output_file.write(i + '\n')
+        output_file.close()     
+    
+    
+
+
+        
+
+if __name__ == '__main__':
+    Parser = Parser()
+    Parser.readline()
+    
+
--- decodificador.vhd
+++ decodificador.vhd
@@ -10,7 +10,7 @@ entity decodificador is
 	port
 	(
    entrada: in STD_LOGIC_VECTOR((largura-1) downto 0);
-	rw : in std_logic;
+	rw : in STD_LOGIC_VECTOR(1 downto 0);
 	habilitasu : out std_logic;
 	habilitasd : out std_logic;
 	habilitamu : out std_logic;
@@ -27,16 +27,16 @@ end entity;
 
 architecture su of decodificador is
     begin
-		  habilitasu <= '1' when entrada = "00000000" and rw = '1' else '0';
-		  habilitasd <= '1' when entrada = "00000001" and rw = '1' else '0';
-		  habilitamu <= '1' when entrada = "00000010" and rw = '1' else '0';
-		  habilitamd <= '1' when entrada = "00000011" and rw = '1' else '0';
-		  habilitahu <= '1' when entrada = "00000100" and rw = '1' else '0';
-		  habilitahd <= '1' when entrada = "00000101" and rw = '1' else '0';
-		  habilitabot1 <= '1' when entrada = "00000111" and rw = '0' else '0';
-		  habilitabot2 <= '1' when entrada = "00001000" and rw = '0' else '0';
-		  habilitabot3 <= '1' when entrada = "00001001" and rw = '0' else '0';
-		  habilitareset <= '1' when entrada = "00001010" and rw = '0' else '0';
-		  basetempo <= '1' when entrada = "00001011" and rw = '0' else '0';
+		  habilitasu <= '1' when entrada = "00000000" and rw = "01" else '0';
+		  habilitasd <= '1' when entrada = "00000001" and rw = "01" else '0';
+		  habilitamu <= '1' when entrada = "00000010" and rw = "01" else '0';
+		  habilitamd <= '1' when entrada = "00000011" and rw = "01" else '0';
+		  habilitahu <= '1' when entrada = "00000100" and rw = "01" else '0';
+		  habilitahd <= '1' when entrada = "00000101" and rw = "01" else '0';
+		  habilitabot1 <= '1' when entrada = "00000111" and rw = "10" else '0';
+		  habilitabot2 <= '1' when entrada = "00001000" and rw = "10" else '0';
+		  habilitabot3 <= '1' when entrada = "00001001" and rw = "10" else '0';
+		  habilitareset <= '1' when entrada = "00001010" and rw = "10" else '0';
+		  basetempo <= '1' when entrada = "00001011" and rw = "10" else '0';
 end architecture;
 
